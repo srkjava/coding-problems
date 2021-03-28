0ec4649b5eede5fd10e1be6154aecb5235d816c5
srkjava <sairamsmart180@gmail.com>
2021-03-28 14:19:52 +0530

    done

--- BankDetails/src/com/sairam/HalloWorld.java
+++ BankDetails/src/com/sairam/HalloWorld.java
@@ -5,7 +5,9 @@ public class HalloWorld {
 	public static void main(String[] args) {
 	
 		System.out.println("bankname");
-			System.out.println("bankname2");
+		System.out.println("bankname2");
+		
+		System.out.println("bankname3");
 				
 
 	}
