faff5eed4e95b2f3bdfe6d93fd84e5b5e3007fab
srkjava <sairamsmart180@gmail.com>
2021-03-28 14:33:19 +0530

    success

--- BankDetails/src/com/sairam/HalloWorld.java
+++ BankDetails/src/com/sairam/HalloWorld.java
@@ -5,11 +5,7 @@ public class HalloWorld {
 	public static void main(String[] args) {
 	
 		System.out.println("bankname");
-		System.out.println("bankname2");
-		
-		System.out.println("bankname3");
-				
-
+	
 	}
 
 }
